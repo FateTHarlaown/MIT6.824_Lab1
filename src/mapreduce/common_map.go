3bf6b02f5393d90cd203fd6fee80bd159640c3ca
FateTHarlaown <623239185@qq.com>
2017-08-08 00:20:46 +0800

    2017.8.8

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,7 +1,10 @@
 package mapreduce
 
 import (
+	"encoding/json"
 	"hash/fnv"
+	"log"
+	"os"
 )
 
 // doMap does the job of a map worker: it reads one of the input files
@@ -14,12 +17,54 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
+	//从这里开始写
+	file, err := os.Open("inFile") // For read access.
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("doMap failed to open input file: ", inFile, "error: ", err)
+	}
+
+	fi, err := file.Stat()
+	if err != nil {
+		log.Fatal("get input file info failed: ", file, "error: ", err)
+	}
+
+	data := make([]byte, fi.Size())
+	_, err = file.Read(data)
+	if err != nil {
+		log.Fatal("read inpufile failed: ", inFile, "error: ", err)
+	}
+
+	kv := mapF(fi.Name(), string(data))
+
+	rFiles := make([]*os.File, nReduce)
+	rEncodes := make([]*json.Encoder, nReduce)
+	for i := 0; i < nReduce; i++ { //创建rReduce个中间文件供reduce步骤读取
+		rFileName := reduceName(jobName, mapTaskNumber, i)
+		rFile, err := os.Create(rFileName)
+		if err != nil {
+			log.Fatal("create middle file failed: ", err)
+		}
+		rFiles[i] = rFile
+		defer rFiles[i].Close()
+		rEncodes[i] = json.NewEncoder(rFiles[i])
+	}
+
+	for _, v := range kv { //遍历经过mapF函数处理得到的中间键值对，分别写入对应的中间文件
+		n := ihash(v.Key) % uint32(nReduce)
+		err = rEncodes[n].Encode(v)
+		if err != nil {
+			log.Fatal("encode kv failed: ", err)
+		}
+	}
 	// TODO:
 	// You will need to write this function.
 	// You can find the filename for this map task's input to reduce task number
 	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
 	// below doMap) should be used to decide which file a given key belongs into.
 	//
+	//下面总的来说就是建议我们用jason来在文件中保存key-value
 	// The intermediate output of a map task is stored in the file
 	// system as multiple files whose name indicates which map task produced
 	// them, as well as which reduce task they are for. Coming up with a
@@ -40,6 +85,7 @@ func doMap(
 	//     err := enc.Encode(&kv)
 	//
 	// Remember to close the file after you have written all the values!
+
 }
 
 func ihash(s string) uint32 {
